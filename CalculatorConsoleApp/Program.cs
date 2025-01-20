3d7f9255a055d0035db5cbadd524b9eb366fd94d
nyinyiminthant-dev <nnyi37389@gmail.com>
2025-01-20 14:54:51 +0630

    Feat: Calculator almost finish

--- CalculatorConsoleApp/Program.cs
+++ CalculatorConsoleApp/Program.cs
@@ -5,30 +5,45 @@ Console.WriteLine("____________________________________");
 Console.WriteLine();
 EfcoreServices ef = new EfcoreServices();
 
-Console.WriteLine("Enter your number1");
-string number1 = Console.ReadLine()!;
-int num1 = Convert.ToInt32(number1);
-Console.WriteLine(" + ,-, * , / ");
-Console.WriteLine();
-string sum = Console.ReadLine()!;
-Console.WriteLine("Enter your number2");
-Console.WriteLine();
-string number2 = Console.ReadLine()!;
-int num2 = Convert.ToInt32(number2);
-Console.WriteLine();
-ef.Calculation(num1,num2,sum);
-ef.ShowOutput();
+int i = 1;
 
-Console.WriteLine("Do you want to saved? (n) or (y)");
-string save = Console.ReadLine()!;
-
-if(save is "Y" || save is  "y")
+void Round()
 {
-    ef.AddResult();
+    Console.WriteLine("Do you want to sart again?");
 }
-else if(save is "N" || save is "n")
+
+while (i == 1 || i > 0)
 {
-    Console.WriteLine("Thanks For Using");
+
+    #region calculator
+
+    Console.WriteLine("Enter your number1");
+    string number1 = Console.ReadLine()!;
+    int num1 = Convert.ToInt32(number1);
+    Console.WriteLine(" + ,-, * , / ");
+    Console.WriteLine();
+    string sum = Console.ReadLine()!;
+    Console.WriteLine("Enter your number2");
+    Console.WriteLine();
+    string number2 = Console.ReadLine()!;
+    int num2 = Convert.ToInt32(number2);
+    Console.WriteLine();
+    ef.Calculation(num1, num2, sum);
+    ef.ShowOutput();
+
+    Console.WriteLine("Do you want to saved? (n) or (y)");
+    string save = Console.ReadLine()!;
+
+    if (save is "Y" || save is "y")
+    {
+        ef.AddResult();
+        
+    }
+    else if (save is "N" || save is "n")
+    {
+        Console.WriteLine("Thanks For Using");
+    }
+    #endregion
 }
 
 Console.ReadLine();
